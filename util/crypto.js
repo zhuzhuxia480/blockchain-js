fe6ac3482ddccaa53c05799560c057d87647151b
zhuzhuxia <742491842@qq.com>
2024-08-06 17:16:41 +0800

    fix crypto double use crash

--- util/crypto.js
+++ util/crypto.js
@@ -1,8 +1,9 @@
 const crypto = require('crypto');
-var hash = crypto.createHash('sha256');
+
 
 class CryptoUtil {
     static hash(data) {
+        let hash = crypto.createHash('sha256');
         let jsonData= typeof data === 'string' ? data.toString() : JSON.stringify(data);
         return hash.update(jsonData).digest('hex')
     }
