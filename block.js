a3897ca11d49a2677526b5acdeaea09792cfa501
zhuzhuxia <742491842@qq.com>
2024-08-07 09:31:48 +0800

    add newBlock function

--- block.js
+++ block.js
@@ -1,4 +1,5 @@
 const CryptoUtil = require("./util/crypto");
+const Proofofwork = require("./proofofwork")
 
 class Block{
     constructor(index, timeStamp, data, preBlockHash){
@@ -15,7 +16,16 @@ class Block{
     }
 
     static newGenesisBlock() {
-        return new Block(0, 1508270000000, "This is Genesis block", "")
+        return this.newBlock(0, 1508270000000, "This is Genesis block", "");
+    }
+
+    static newBlock(index, timeStamp, data, preBlockHash) {
+        let block = new Block(index, timeStamp, data, preBlockHash);
+        let pow = new Proofofwork(block);
+        let ret = pow.run();
+        block.hash = ret[0];
+        block.nonce = ret[1];
+        return block;
     }
 }
 
