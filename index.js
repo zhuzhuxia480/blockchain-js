5bb2df94da5415ee7565691b9b5c86754a6ba9d3
zhuzhuxia <742491842@qq.com>
2024-08-07 08:19:52 +0800

    print blockchain data in index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const Blockchain = require("./blockchain")
 const Proofofwork = require("./proofofwork")
 
 
-
 const blockchain = new Blockchain();
-const proofofwork = new Proofofwork(blockchain.blocks[0]);
-proofofwork.run();
+blockchain.addBlock("send 1 BTC to Ivan")
+blockchain.addBlock("send 2 more BTC to Ivan")
+
+for (const block of blockchain.blocks) {
+    console.log("Pre.hash:", block.preBlockHash);
+    console.log("Data:", block.data);
+    console.log("Hash:", block.hash);
+    console.log();
+}
+
