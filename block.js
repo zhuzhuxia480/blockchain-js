57bed8eef0c1bfb7117c1920edcaae56bec61e7f
zhuzhuxia <742491842@qq.com>
2024-08-13 11:29:55 +0800

    add transactions in block

--- block.js
+++ block.js
@@ -2,25 +2,25 @@ const CryptoUtil = require("./util/crypto");
 const Proofofwork = require("./proofofwork")
 
 class Block{
-    constructor(index, timeStamp, data, preBlockHash){
+    constructor(index, timeStamp, txs, preBlockHash){
         this.index = index;
         this.timeStamp = timeStamp;
-        this.data = data;
+        this.transactions = txs;
         this.preBlockHash = preBlockHash;
         this.hash = this.calculateHash()
         this.nonce = 0;
     }
 
     calculateHash() {
-        return CryptoUtil.hash(this.preBlockHash + this.data + this.timeStamp)
+        return CryptoUtil.hash(this.preBlockHash + JSON.stringify(this.transactions) + this.timeStamp)
     }
 
     static newGenesisBlock() {
         return this.newBlock(0, 1508270000000, "This is Genesis block", "");
     }
 
-    static newBlock(index, timeStamp, data, preBlockHash) {
-        let block = new Block(index, timeStamp, data, preBlockHash);
+    static newBlock(index, timeStamp, txs, preBlockHash) {
+        let block = new Block(index, timeStamp, txs, preBlockHash);
         let pow = new Proofofwork(block);
         let ret = pow.run();
         block.hash = ret[0];
